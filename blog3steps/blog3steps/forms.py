3ecf3b94785165098e036ffbe262cd137aac0fd5
Lorenzo Pascucci <lorenzo.pasa@gmail.com>
2014-05-29 00:59:50 +0200

    Added a check to avoid more Blog instances having the same URL. Now the Appstats console is accessible only by admin users.

--- blog3steps/blog3steps/forms.py
+++ blog3steps/blog3steps/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm, ValidationError, widgets
 from django.utils.translation import ugettext as _
+from google.appengine.api import users
 
 from .models import Article, Blog
 
@@ -23,10 +24,20 @@ class BlogForm(ModelForm):
         fields = ('url', 'title',)
 
     def clean_url(self):
-    # Use the same regex used by the URLconf for matching the public blog URLs
+        """
+        Use the same regex used by the URLconf for matching the public blog URLs.
+        """
         data = self.cleaned_data['url'].lower()
         check = re.match(r'^([a-z0-9]+[-]*)+[a-z0-9]+$', data)
         if not check:
             raise ValidationError(_("Only lowercase letters, numbers, or hyphens are admitted. It cannot start or end with a hyphen."))
+        
+        # Check if another Blog with the same URL (but belonging to a different user) already exists
+        already_exists = Blog.objects.filter(url=data)
+        if already_exists:
+            user_id = users.get_current_user().user_id()
+            user_is_owner = already_exists.filter(administrator__in=[user_id])
+            if not user_is_owner:
+                raise ValidationError(_("A Blog with this URL already exists. Please choose another URL."))
         return data
 
